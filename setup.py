cbb5bdf8bc9c32d4d80e6e1b7f87346f9c1c85bc
Alex Betty <alex@cognitiv.ai>
2018-03-03 17:05:07 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='rk-launcher-aws',
+    version='0.0.1',
+    packages=['rklauncher'],
+    url='github.com:alex-cognitiv/rk-launcher-aws.git',
+    license='Apache 2.0',
+    author='alex',
+    author_email='alex@cognitiv.ai',
+    description=''
+)
